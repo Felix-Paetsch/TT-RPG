9e24e1fe82e1ed7acd64269a783c9419c4346f18
Felix <56132896+Felix-Paetsch@users.noreply.github.com>
2024-05-06 21:54:23 +0200

    Fill in blanks

--- kernel/extensible.py
+++ kernel/extensible.py
@@ -1,26 +1,45 @@
 class Extensible:
     def __init__(self):
-        self.plugins   = {}
-        self.preloaded_plugins = []
+        self.plugins   = {}         #   "name": Plugin
+        self.preloaded_plugins = [] # (Plugin, position)[]
 
+    # >>> Serialize everything that belongs to the Extensible, 
+    # >>> Will be done in plugin.py, world.py
     def serealize(self):
         raise Exception("Unimplemented!")
 
     def deserealize(self, obj):
         raise Exception("Unimplemented!")
+    # <<<
+    # <<<
 
-    def serealize_self(self):
-        raise Exception("Unimplemented!")
+    # >>> Serialize the plugins of the Extensible
+    # >>> This specifically excludes the plugin itself
+    def serealize_plugins(self):
+        return {
+            key: value.serealize()
+            for key, value in self.plugins.items()
+        }
 
-    def deserealize_self(self, obj):
-        raise Exception("Unimplemented!")
+    def deserealize_plugins(self, obj):
+        for key, value in obj.items():
+            self.plugins[key].deserealize(value)
+    # <<<
+    # <<<
+
+    def initialize_self(self):
+        pass
 
     def initialize(self):
-        raise
+        self.initialize_self()
+        self.preloaded_plugins.sort(key=lambda x: x[1])
+        for (plugin, _) in self.preloaded_plugins:
+            plugin.initialize_self()
 
     def plugin(self, plugin, position = 0):
-        self.preloaded_plugins.append(plugin)
+        self.preloaded_plugins.append((plugin, position))
+        plugin.extends_on(self)
         return self
 
-    def if_plugin(plugin_name):
-        raise
+    def if_plugin(self, plugin_name, _):
+        return plugin_name in self.plugins
--- kernel/plugin.py
+++ kernel/plugin.py
@@ -1,9 +1,46 @@
 class Plugin(Extensible):
     def __init__(self, name):
-        self.name = name
+        self.name    = name
+        self.extends = None
 
     def serealize(self):
-        raise
+        return {
+            "name": self.name,
+            "sub_plugins": self.serealize_plugins(),
+            "self": self.serealize_self()
+        }
     
     def deserealize(self, obj):
-        raise
+        self.deserealize_self(obj["self"])
+        self.deserealize_plugins(obj["sub_plugins"])
+
+    def extends_on(self, extends):
+        self.extends = extends
+
+    def if_plugin(self, plugin_name, propagation):
+        if plugin_name in self.plugins:
+            return True
+
+        if propagation == 0:
+            return False
+
+        return self.extends.if_plugin(plugin_name, propagation - 1)
+
+    # >>> The following should be overwritten by plugin
+    # >>>
+    def serealize_self(self):
+        # Default:
+        ### return dict()
+        raise Exception("Unimplemented!")
+
+    def deserealize_self(self, obj):
+        # Default:
+        ### pass
+        raise Exception("Unimplemented!")
+
+    def initialize_self(self):
+        # Default:
+        ### pass
+        raise Exception("Unimplemented!")
+    # <<<
+    # <<<
